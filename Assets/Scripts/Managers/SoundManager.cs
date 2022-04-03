1c3b8b8c0c9b69808b377fc646a1d70a11494702
Bartosz383 <kruszynskibartosz1@gmail.com>
2022-04-03 20:22:50 +0200

    add

--- Assets/Scripts/Managers/SoundManager.cs
+++ Assets/Scripts/Managers/SoundManager.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.Serialization;
-
+    ...
 public class SoundManager : MonoBehaviour
 {
     public Sound[] sounds;
