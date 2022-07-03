e1d60f3d05a486403f4daf5642aa4c0929dd93c8
Wojtek Czerwinski <czerwinskiwtj@gmail.com>
2022-07-03 15:50:47 +0200

    Ranged Enemy

---
+++ Assets/Scripts/Enemy/RangedEnemyController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class RangedEnemyController : EnemyControllerBase
+{
+    [SerializeField] private float attackRange;
+
+    private bool canShoot = true;
+    
+    protected override void FixedUpdate()
+    {
+        base.FixedUpdate();
+    }
+
+    protected override void MoveTowardsPlayer()
+    {
+        var dir = movementSpeed * Vector3.Normalize(playerTransform.position - transform.position);
+        rb.velocity = new Vector3(dir.x, rb.velocity.y, dir.z);
+        if (Vector3.Distance(playerTransform.position, transform.position) > attackRange) return;
+        rb.velocity = Vector3.zero;
+    }
+}
