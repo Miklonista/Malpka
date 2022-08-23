23ce3c76edc104a1916ff82fba491cd069191b96
Wojciech Czerwinski <wojciech.czerwinski@put.poznan.pl>
2022-08-23 23:44:49 +0200

    boss spawning smaller enemies - logic implementation using unity.random

--- Assets/Boss.cs
+++ Assets/Boss.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Boss : MonoBehaviour
 {
@@ -18,7 +19,7 @@ public class Boss : MonoBehaviour
 
     private void Update()
     {
-
+        SpawnMob();
         if (hearts.Count != 0) return;
         Die();
     }
@@ -28,9 +29,23 @@ public class Boss : MonoBehaviour
         Debug.Log("here");
         hearts.RemoveAt(hearts.Count - 1);
     }
+
+    private void SpawnMob()
+    {
+        int result = Random.Range(0, 2); // 0 - melee, 1 - ranged
+
+        if (result > 1) return;
+
+        Action spawnMelee = () => { };
+        Action spawnRanged = () => { };
+
+        var func = result == 0 ? spawnMelee : spawnRanged;
+        func(); // just testing ternary operator and lambda possibilities, its sick but shit
+    }
     
     private void Die()
     {
+        // add a reward 
         Destroy(gameObject);
     }
 }
